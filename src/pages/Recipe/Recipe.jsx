c07298468680786a2367754b33aa816ec4c5def7
nsr2020 <146729141+nsr2020@users.noreply.github.com>
2024-03-22 16:19:36 +0100

    changes in notFound file

--- src/pages/Recipe/Recipe.jsx
+++ src/pages/Recipe/Recipe.jsx
@@ -4,30 +4,39 @@ import { Link, useParams } from "react-router-dom";
 import Loading from "../../components/Loading/Loading";
 import GoBack from "../../components/GoBack/GoBack";
 import RecipeInfoCard from "../../components/RecipeInfoCard/RecipeInfoCard";
+import Notfound from "../NotFound/Notfound";
 
 const Recipe = () => {
 
     const { id } = useParams();
-    const [recipe, setRecipe] = useState(); // Asegúrate de inicializar recipe con null en lugar de undefined
+    const [recipe, setRecipe] = useState(); 
     const [loading, setLoading] = useState(true);
+    const [notFound, setNotFound] = useState(false);
 
     useEffect(() => {
         setLoading(true);
+        setNotFound(false);
         setTimeout(() => {
             fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
                 .then(response => response.json())
                 .then(data => {
                     if (data.meals && data.meals.length > 0) {
-                        setRecipe(data.meals[0]); // 
+                        setRecipe(data.meals[0]);
+                    } else {
+                        setNotFound(true); 
                     }
-                    setLoading(false); // 
+                    setLoading(false);
                 })
                 .catch(error => {
                     console.error('Error fetching recipe:', error);
                     setLoading(false);
-                }); // Aquí estaba faltando el cierre de paréntesis
+                });
         }, 1000);
     }, []);
+
+    if (notFound) {
+        return <Notfound/>;
+    }
 
     return (
         <section className="recipe-container flex-container">
@@ -36,8 +45,7 @@ const Recipe = () => {
             )}
             <Link to={`/Recipes/${recipe?.strArea}`}>
             <GoBack/>
-            </Link>
-            
+            </Link> 
         </section>
     );
 }
